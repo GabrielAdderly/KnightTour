b52f81b50a4da3b458623e55afe229a04151feaf
highjellybon <highjellybon@gmail.com>
2019-12-19 02:44:54 +0200

    Добавьте файлы проекта.

---
+++ KnightTour/Menu.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading;
+
+namespace KnightTour
+{
+    class Menu
+    {
+        private static int N = 7;
+
+        private static int getSize()
+        {
+            Console.Clear();
+            Console.Write("Enter board size NxN (N > 5 & N <= 10) : ");
+            int num;
+            try
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num < 6 || num > 10)
+                {
+                    Console.WriteLine("You have entered a number that is less than 6 or higher than 10");
+                    Thread.Sleep(1000);
+                    return getSize();
+                }
+                else
+                    return num;
+            }
+            catch (NotFiniteNumberException ex)
+            {
+                Console.WriteLine("You entered invalid symbol!");
+                Thread.Sleep(1000);
+                return getSize();
+            }
+        }
+
+        internal static int[,] GetFieldOptions()
+        {
+            int[,] field;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            N = getSize();
+            return field = new int[N, N];
+
+        }
+
+        internal static void Show(int[,] field)
+        {
+            bool end = true;
+            int num = 1;
+
+            while (end)
+            {
+                Console.Clear();
+
+                Console.WriteLine("\n\n");
+                for (int row = 0; row < field.GetLength(0); row++)
+                {
+                    Console.Write("\t");
+                    for (int col = 0; col < field.GetLength(1); col++)
+                    {
+                        if (field[row, col] <= num)
+                        {
+                            if (field[row, col] == 1 || field[row, col] == field.Length)
+                                Console.ForegroundColor = ConsoleColor.Red;
+                            else if (field[row, col] < 10)
+                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                            else if (field[row, col] < 20)
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            else if (field[row, col] < 30)
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                            else if (field[row, col] < 40)
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                            else if (field[row, col] < 50)
+                                Console.ForegroundColor = ConsoleColor.Magenta;
+                            else if (field[row, col] < 60)
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                            else if (field[row, col] < 70)
+                                Console.ForegroundColor = ConsoleColor.White;
+                            else if (field[row, col] < 80)
+                                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                            else if (field[row, col] < 90)
+                                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                            else if (field[row, col] < 100)
+                                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                            else
+                                Console.ForegroundColor = ConsoleColor.Black;
+
+                            if (field[row, col] < 10)
+                                Console.Write("  " + field[row, col] + " ");
+                            else if (field[row, col] < 100)
+                                Console.Write(" " + field[row, col] + " ");
+                            else
+                                Console.Write(field[row, col] + " ");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.Write("  0 ");
+                        }
+                        if (num == field.Length + 1)
+                            end = false;
+
+                    }
+                    Console.WriteLine("\n");
+                }
+                num++;
+                Thread.Sleep(500);
+            }
+            Console.ReadKey(true);
+        }
+
+    }
+}
---
+++ KnightTour/Start.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightTour
+{
+    class Start
+    {
+        private const int MAX_NUM_OF_MOVES = 8;
+        private static int[,] field;
+        private static int[,] optionsMoves = { { 2, 1 }, { 2, -1 }, { -2, 1 }, { -2, -1 },
+                                                { 1, 2 }, { 1, -2 }, { -1, 2 }, { -1, -2 } };
+        private static int currentX;
+        private static int currentY;
+        private static int count;
+        private static Random rand = new Random();
+        static int Num = 1000;
+
+        internal static void Run()
+        {
+            field = Menu.GetFieldOptions();
+            count = 1;
+
+            int startY = rand.Next(field.GetLength(0));
+            int startX = rand.Next(field.GetLength(1));
+            currentX = startX;
+            currentY = startY;
+
+            while (true)
+            {
+                field[currentY, currentX] = count;
+
+                if (count == field.Length)
+                {
+                    ChangeByEulerMethod(count);
+                    break;
+                }
+                int i = GetRightMove();
+
+                if (i == 9)
+                {
+                    FillEmptyCells();
+                    ChangeByEulerMethod(count, false);
+                    break;
+                }
+
+                currentY += optionsMoves[i, 0];
+                currentX += optionsMoves[i, 1];
+                count++;
+            }
+
+            Menu.Show(field);
+
+        }
+
+        #region Euler Method
+
+        static void ChangeByEulerMethod(int endNum, bool isEnded = true)
+        {
+            int num = 0;
+            if (!isEnded)
+            {
+                for (int k = Num; k >= 1000; k--)
+                {
+                    num = GetTheRightPoint(k, endNum);
+                    ChangeLastPaths(num, endNum);
+                    ChangeNum(k, ++endNum);
+                }
+                if (num == -1)
+                    return;
+                ChangeLastPaths(num, endNum);
+            }
+
+            num = GetTheRightPoint(1, endNum);
+            if (num == -1)
+                return;
+
+            ChangeLastPaths(num, endNum);
+
+        }
+
+        static void ChangeNum(int num, int modifiedNum)
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+                for (int j = 0; j < field.GetLength(1); j++)
+                    if (field[i, j] == num)
+                        field[i, j] = modifiedNum;
+        }
+
+        static void FillEmptyCells()
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+                for (int j = 0; j < field.GetLength(1); j++)
+                    if (field[i, j] == 0)
+                        field[i, j] = Num++;
+
+            Num--;
+        }
+
+        static int GetTheRightPoint(int startPoint, int finalPoint)
+        {
+            List<int> aroundTheStartPoint = GetTheNearestPoints(startPoint);
+            List<int> aroundTheFinalPoint = GetTheNearestPoints(finalPoint);
+            aroundTheFinalPoint.Sort();
+            aroundTheStartPoint.Sort();
+            int num = FindingASuitablePoint(aroundTheStartPoint, aroundTheFinalPoint);
+
+            if (finalPoint == field.Length && num == finalPoint)
+                return -1;
+
+            return num;
+        }
+
+        private static List<int> GetTheNearestPoints(int num)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < field.GetLength(0); i++)
+                for (int j = 0; j < field.GetLength(1); j++)
+                    if (field[i, j] == num)
+                        for (int k = 0; k < optionsMoves.GetLength(0); k++)
+                            if (isNotNull((i + optionsMoves[k, 0]), (j + optionsMoves[k, 1])))
+                                list.Add(field[i + optionsMoves[k, 0], j + optionsMoves[k, 1]]);
+
+            return list;
+        }
+
+        private static int FindingASuitablePoint(List<int> firstNeighbors, List<int> secondNeighbors)
+        {
+            for (int i = firstNeighbors.Count - 1; i >= 0; i--)
+                for (int j = secondNeighbors.Count - 1; j >= 0; j--)
+                {
+                    if (firstNeighbors[i] == secondNeighbors[j]
+                        || firstNeighbors[i] == (secondNeighbors[j] + 1)
+                        || firstNeighbors[i] == (secondNeighbors[j] - 1))
+                    {
+                        return firstNeighbors[i];
+                    }
+                }
+            return 0;
+        }
+
+        private static void ChangeLastPaths(int num, int maxNum)
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+                for (int j = 0; j < field.GetLength(1); j++)
+                    if (field[i, j] >= num && field[i, j] < Num)
+                    {
+                        int k = field[i, j] - num;
+                        field[i, j] = maxNum - k;
+
+                    }
+        }
+
+        #endregion
+
+        #region The Warnsdorf’s rule
+        //Method for find 
+        static int GetRightMove()
+        {
+            int[] arr = new int[MAX_NUM_OF_MOVES];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = CountOfNextVariation(currentY + optionsMoves[i, 0], currentX + optionsMoves[i, 1]);
+            }
+
+            int min = MAX_NUM_OF_MOVES + 1;
+            for (int j = 0; j < arr.Length; j++)
+            {
+                if (arr[j] != 0 && arr[j] < min)
+                    min = arr[j];
+            }
+
+            if (min == MAX_NUM_OF_MOVES + 1)
+                min = 0;
+
+            List<int> rightMoves = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == min)
+                    rightMoves.Add(i);
+            }
+            if (rightMoves.Count > 0)
+                return rightMoves[0];
+            else
+                return MAX_NUM_OF_MOVES + 1;
+        }
+
+        //Method for counting the number of possible paths
+        static int CountOfNextVariation(int y, int x)
+        {
+            int counter = 0;
+
+            if (isNotNullorNotFilled(y, x))
+            {
+                for (int i = 0; i < optionsMoves.GetLength(0); i++)
+                {
+                    if (isNotNullorNotFilled((y + optionsMoves[i, 0]), (x + optionsMoves[i, 1])))
+                        counter++;
+                }
+                return counter;
+            }
+
+            return MAX_NUM_OF_MOVES + 1;
+        }
+
+        static bool isNotNullorNotFilled(int y, int x)
+        {
+
+            if (x >= 0 && y >= 0 && y < field.GetLength(0) && x < field.GetLength(1) && field[y, x] == 0)
+                return true;
+
+            return false;
+        }
+
+        static bool isNotNull(int y, int x)
+        {
+
+            if (x >= 0 && y >= 0 && y < field.GetLength(0) && x < field.GetLength(1))
+                return true;
+
+            return false;
+        }
+
+        #endregion
+
+    }
+}
